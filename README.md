6126335a8d8c0fa23b0956876c9109dcd0692d86
Jenna-Lee Shearer <shearerjennalee@gmail.com>
2024-11-14 13:39:43 +0100

    setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-weather
+
+### `npm start`
---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import './Weather.css';
+
+function Weather() {
+  return (
+    <div className="Weather">
+      <h1>React Weather App</h1>
+    </div>
+  );
+}
+
+export default Weather;
