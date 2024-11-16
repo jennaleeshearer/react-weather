7b42d2d260a9fe5a995e7276f5ef0e81c2e531dc
Jenna-Lee Shearer <shearerjennalee@gmail.com>
2024-11-16 13:24:33 +0100

    api

--- src/Weather.js
+++ src/Weather.js
@@ -2,17 +2,41 @@ import './Weather.css';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import WeatherNavbar from './navbar';
+import WeatherDisplay from './displayWeather';
+import React, { useState } from 'react';
 
 function Weather() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
   return (
     <div className="Weather">
       <WeatherNavbar />
       <h1 className="mt-5">React Weather App</h1>
 
-      <div className="container">
-        <InputGroup className="mb-3">
-          <Form.Control id="location-input" placeholder="Enter your Location"/>
+      <div className="container d-flex">
+        <InputGroup className="mb-3 me-3">
+          <Form.Control
+              id="latitude"
+              placeholder="latitude"
+              value={latitude}
+              onChange={(e) => setLatitude(e.target.value)}
+          />
         </InputGroup>
+        <InputGroup className="mb-3">
+          <Form.Control
+              id="longitude"
+              placeholder="longitude"
+              value={longitude}
+              onChange={(e) => setLongitude(e.target.value)}
+          />
+        </InputGroup>
+
+
+      </div>
+
+      <div id="displayWeather">
+        <WeatherDisplay latitude={latitude} longitude={longitude} />
       </div>
     </div>
   );
